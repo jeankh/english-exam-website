562d2090f36c88ac3e72c02272265d3ad38090f2
Jean Khoge <khogejean@gmail.com>
2023-12-19 09:32:17 +0100

    create Darl/Light Mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import { Route, Routes } from "react-router-dom";
+import "./reset.css";
+import "./App.css";
+import { pages } from "./data/data";
 
 function App() {
+  const getPreferredTheme = () => {
+    return window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  };
+
+  const initialTheme = localStorage.getItem("theme") || getPreferredTheme();
+  const [theme, setTheme] = useState(initialTheme);
+
+  // const toggleTheme = () => {
+  //   const newTheme = theme === "light" ? "dark" : "light";
+  //   setTheme(newTheme);
+  //   localStorage.setItem("theme", newTheme);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <h1>Hello !</h1>
+      {/* <Header theme={theme} onChangeTheme={toggleTheme} pages={pages} /> */}
+      {/* <Routes> */}
+      {/* <Route
+          path="/"
+          element={
+            <Home theme={theme} aboutMeData={aboutMeData} projects={projects} />
+          }
+        />
+        <Route
+          path="/about-me"
+          element={<AboutMe theme={theme} aboutMeData={aboutMeData} />}
+        />
+        <Route
+          path="/my-projects"
+          element={<Projects theme={theme} projects={projects} />}
+        /> */}
+      {/* </Routes> */}
     </div>
   );
 }
---
+++ src/data/data.jsx
@@ -0,0 +1,17 @@
+export const pages = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "Players",
+    link: "/players",
+  },
+  {
+    id: 3,
+    label: "player",
+    link: "/player",
+  },
+];
